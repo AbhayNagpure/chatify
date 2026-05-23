1490f47eb512b0945e5f28ddbeb012c52396b447
Abhay Nagpure <abhaynagpure99@gmail.com>
2026-05-23 11:40:04 +0530

    added cloudinary functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -86,3 +86,27 @@ export const logout = (_ , res) => {
     res.cookie("jwt", "", {maxAge:0})
     res.status(200).json({message: "You have logged out successfully"})
 }
+
+export const updateProfile = async (req, res) => {
+    try {
+        const profilePic = req.body;
+        if(!profilePic) return res.status(400).json({message: "Profile pic is required!"})
+        
+        const userId = req.user._id;
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+        const updatedUser = await User.findByIdAndUpdate(  //puted cloudinary url into database.
+            userId,  
+            {profilePic : uploadResponse.secure_url},
+            {new: true}
+        );
+        res.status(200).json(updatedUser);
+
+
+
+    } catch (error) {
+        console.log("Error in updating profile pic:", error)
+        res.status(500).json({message: "Internal server error!"})
+    }
+}
