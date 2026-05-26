176db2114b9f238ba8482f655fb24c6e59ca1c3c
Abhay Nagpure <abhaynagpure99@gmail.com>
2026-05-26 14:51:48 +0530

    signup page UI implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import messageRoutes from "./routes/message.route.js";
 import path from "path";
 import {connectDB} from "./lib/db.js"
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express()                                          //create an express 'application instance'
 const __dirname = path.resolve()
@@ -13,7 +14,7 @@ const __dirname = path.resolve()
 const PORT =ENV.PORT || 3000
 app.use(express.json())                                       //MIDDLEWARE: parse JSON request bodies.
 app.use(cookieParser())
-
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true}));
 app.use("/api/auth", authRoutes)                               // 4. ROUTING: All /api/auth/* → authRoutes
 app.use("/api/messages", messageRoutes)                       //  ROUTING: All /api/messages/* → messageRoutes
 
